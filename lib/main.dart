4ff5f9ce18bfc63ddb2e8e78e7f307b45f9cbcdd
Shivam Chauhan <cs.vic125@gmail.com>
2023-05-19 13:37:26 +0530

    Added welcome sequence

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sharptrack/screens/homepage.dart';
+import 'package:sharptrack/screens/splashscreen.dart';
 import 'package:sharptrack/skeleton.dart';
 import 'package:flutter_background/flutter_background.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
             const AppBarTheme(backgroundColor: Colors.black, centerTitle: true),
       ),
       title: '#Track',
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
